3451042ad5dbccbc15a9cd1aada381359a644da0
Praveen Sahu <sahupraveen101@gmail.com>
2022-05-02 14:23:15 +0530

    loadmore, open recommended products-in progress

--- src/SelfCheckout/components/RecommendedProduct.js
+++ src/SelfCheckout/components/RecommendedProduct.js
@@ -15,34 +15,69 @@ export default class RecommendedProduct extends Component {
     {
 
     }
-    componentWillMount() {
-     var ids="";//"873,1870,1702,1893,1895";
-     if(this.props.item && this.props.page)
-     {
-         ids=this.props.item.ReletedIds;
-     }
-     else
-     {
-        ids = getRecommendedProducts(_key.PRODUCT_RECOMMENDATIONS,"cart");
-     }
     
-     var idbKeyval = FetchIndexDB.fetchIndexDb();
-        idbKeyval.get('ProductList').then(val => {
-            if (!val || val.length == 0 || val == null || val == "") {
-            } 
-            else
-            {
-                var _productwithTax = getTaxAllProduct(val)
-                var productlist = _productwithTax;
-                const filter_products = productlist && productlist.filter(item =>{
-                return ids.includes(`${item.WPID}`)
-                });
-                // console.log("---getRecommendedProducts-"+JSON.stringify(filter_products));
-                this.setState({productList:filter_products});
-            }
-        });
+    componentWillMount() {
+    //  var ids="";//"873,1870,1702,1893,1895";
+    //  if(nextPros.item && nextPros.page)
+    //  {
+    //      ids=nextPros.item.ReletedIds;
+    //  }
+    //  else
+    //  {
+    //     ids = getRecommendedProducts(_key.PRODUCT_RECOMMENDATIONS,"cart");
+    //  }
+    // if(ids!="")
+    // {
+    //  var idbKeyval = FetchIndexDB.fetchIndexDb();
+    //     idbKeyval.get('ProductList').then(val => {
+    //         if (!val || val.length == 0 || val == null || val == "") {
+    //         } 
+    //         else
+    //         {
+    //             var _productwithTax = getTaxAllProduct(val)
+    //             var productlist = _productwithTax;
+    //             const filter_products = productlist && productlist.filter(item =>{
+    //             return ids.includes(`${item.WPID}`)
+    //             });
+    //             // console.log("---getRecommendedProducts-"+JSON.stringify(filter_products));
+    //             this.setState({productList:filter_products});
+    //         }
+    //     });
+    // }
     //  console.log("---_productData-"+JSON.stringify(_productData))
     //  this.setState({productList:_productData});
+    }
+    getProducts(nextPros)
+    {
+        var ids="";
+        if(nextPros.item && nextPros.page)
+        {
+            ids=nextPros.item.ReletedIds;
+        }
+        else
+        {
+            ids = getRecommendedProducts(_key.PRODUCT_RECOMMENDATIONS,"cart");
+        }
+        if(ids!="")
+        {
+        var idbKeyval = FetchIndexDB.fetchIndexDb();
+            idbKeyval.get('ProductList').then(val => {
+                if (!val || val.length == 0 || val == null || val == "") {
+                } 
+                else
+                {
+                    var _productwithTax = getTaxAllProduct(val)
+                    var productlist = _productwithTax;
+                    const filter_products = productlist && productlist.filter(item =>{
+                    return ids.includes(`${item.WPID}`)
+                    });
+                    this.setState({productList:filter_products});
+                }
+            });
+        }
+    }
+    componentWillReceiveProps(nextPros) {
+        this.getProducts(nextPros);
     }
    render() {
        const {productList} = this.state;
@@ -51,34 +86,34 @@ export default class RecommendedProduct extends Component {
         <p>Recommendations</p>
         <div className="row">
         {    
-                (productList && productList.length !=0)?
-                    productList && productList.map((item, index) => {
-                        var display_expireTicketTime;
-                        var isVariableProduct = (item.Type !== "simple" && item.StockStatus !== "outofstock") ? true : false;
-                        if (item.IsTicket && item.IsTicket == true) {
-                            var ticketInfo = JSON.parse(item.TicketInfo);
-                            if (ticketInfo._ticket_availability.toLowerCase() == 'range' && (ticketInfo._ticket_availability_to_date)) {
-                                var dt = new Date(ticketInfo._ticket_availability_to_date);
-                                display_expireTicketTime = moment(dt).format('LT');
-                            }
+            (productList && productList.length !=0)?
+                productList && productList.map((item, index) => {
+                    var display_expireTicketTime;
+                    var isVariableProduct = (item.Type !== "simple" && item.StockStatus !== "outofstock") ? true : false;
+                    if (item.IsTicket && item.IsTicket == true) {
+                        var ticketInfo = JSON.parse(item.TicketInfo);
+                        if (ticketInfo._ticket_availability.toLowerCase() == 'range' && (ticketInfo._ticket_availability_to_date)) {
+                            var dt = new Date(ticketInfo._ticket_availability_to_date);
+                            display_expireTicketTime = moment(dt).format('LT');
                         }
-                        return (  
-                            <div className="item" key={"product_"+index}>
-                                <img src={item.ProductImage ? item.ProductImage : 'placeholder.png'} alt="new" onError={(e) => imgError(e.target)}   className="scale"/>
-                                <p className="prod-name">{item.Title ? item.Title : item.Sku ? item.Sku : 'N/A'}</p>
-                                <p className="price">starting at $ {item.Price}</p>
-                                <button key={index}
-                            data-toggle={isVariableProduct ? "modal" : ""} href="javascript:void(0)">View Item</button>
-                            </div>
-                        )
-                    })
-                :
-                    <div className="w-100">                                    
-                        <p className="text-center payment-description">
-                        {LocalizedLanguage.noMatchingProductFound}
-                        </p>
-                    </div>
-                }    
+                    }
+                    return (  
+                        <div className="item" key={"product_"+index}>
+                            <img src={item.ProductImage ? item.ProductImage : 'placeholder.png'} alt="new" onError={(e) => imgError(e.target)}   className="scale"/>
+                            <p className="prod-name">{item.Title ? item.Title : item.Sku ? item.Sku : 'N/A'}</p>
+                            <p className="price">starting at $ {item.Price}</p>
+                            <button key={index}
+                        data-toggle={isVariableProduct ? "modal" : ""} href="javascript:void(0)" onClick={()=>this.props.handleSimpleProduct?this.props.handleSimpleProduct(item):this.props.handleProductData(item)}> View Item</button>
+                        </div>
+                    )
+                })
+            :
+                <div className="w-100">                                    
+                    <p className="text-center payment-description">
+                    {LocalizedLanguage.noMatchingProductFound}
+                    </p>
+                </div>
+            }    
         </div>
     </div>
     )
