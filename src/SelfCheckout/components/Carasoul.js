9fe4ece477cc5ebfe55d1c2ed51afa99a36a80a6
Praveen Sahu <sahupraveen101@gmail.com>
2022-05-04 16:53:35 +0530

    Update Carasoul.js

--- src/SelfCheckout/components/Carasoul.js
+++ src/SelfCheckout/components/Carasoul.js
@@ -5,26 +5,16 @@ const Carasoul=(props)=> {
 	{
     	return (
 		<div className="slider-container m-b-35">
-				<div className="slider">
-						{props.banners.map((element, index) => {
-							<img src={Config.key.RECIEPT_IMAGE_DOMAIN +element.Path} alt="" />
-							// <li key={index} style={{backgroundImage: "url("+Config.key.RECIEPT_IMAGE_DOMAIN +element.Path+")"}}>
-							// <div className="center-y"></div>
-							// </li>
-						})}
-						{/* <li>
-						<div className="center-y"></div>
-						</li> */}
-					{/* <ul>
-						<nav>
-							<a href="#"></a>
-							<a href="#"></a>
-							<a href="#"></a>
-							<a href="#"></a>
-						</nav>
-					</ul> */}
-					<div className="slider-toggles"></div>
-				</div>
+			<div className="slider">
+				{
+				props.banners.map((element, index) => {
+					return(
+					<img src={Config.key.RECIEPT_IMAGE_DOMAIN +element.Path} alt="" />
+					)
+				 })}
+			</div>
+			<div className="slider-toggles">
+			</div>
 		</div>)
 	}
 }
