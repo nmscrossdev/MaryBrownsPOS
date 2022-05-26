f638349e556b49a6dfa950797737deecea7d6061
Praveen Sahu <sahupraveen101@gmail.com>
2022-05-26 12:48:31 +0530

    apple login

--- src/_components/CommonProductPopupModal.js
+++ src/_components/CommonProductPopupModal.js
@@ -1537,7 +1537,7 @@ class CommonProductPopupModal extends React.Component {
                                             <div className="inner-row">
                                                 <div className="text-row">
                                                     <p className="price"><NumberFormat value={tax_is && RoundAmount(((product_price * this.state.variationDefaultQunatity) - after_discount_total_price) + (tax_is.excl_tax ? tax_is.excl_tax : 0))} displayType={'text'} thousandSeparator={true} decimalScale={2} fixedDecimalScale={true} /></p>
-                                                    <p className="subtext">  (plus tax)</p>
+                                                    <p className="subtext">({typeOfTax() == 'incl' ? LocalizedLanguage.inclTax : "excl "+LocalizedLanguage.exclTax})</p>
                                                 </div>
                                                 {/* <button type="button">Modify Ingredients</button> */}
                                             </div>
