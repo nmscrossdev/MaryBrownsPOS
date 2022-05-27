f6073bb4d231543e634c7bd2ea48309bdec64538
nmscrossdev <78338383+nmscrossdev@users.noreply.github.com>
2022-05-27 18:19:13 +0530

    variation popup edit

--- src/_components/views/PopupDisplayMessage.js
+++ src/_components/views/PopupDisplayMessage.js
@@ -15,24 +15,40 @@ class PopupDisplayMessage extends React.Component {
         hideModal('popupDisplayMessage');
     }
 
-    
-   
+
+
 
 
     render() {
-        console.log(this.props,"variation popup")
+        console.log(this.props, "variation popup")
         return (
+            // <div className="popup hide" id="popupDisplayMessage" style={{overflowY:"hidden",textAlign:"center"}}>
             <div className="popup hide" id="popupDisplayMessage" style={{overflowY:"hidden",textAlign:"center"}}>
-                <div className="product-container">
+                    <div className="product-container">
+                <div type="button" className="popup-close">
+                    <svg onClick={() => this.closePopupDisplayMessage()}
+                        width="22"
+                        height="21"
+                        viewBox="0 0 22 21"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M19.0466 21L10.7521 12.9L2.45762 21L0 18.6L8.29448 10.5L0 2.4L2.45762 0L10.7521 8.1L19.0466 0L21.5042 2.4L13.2097 10.5L21.5042 18.6L19.0466 21Z"
+                            fill="#050505"
+                        />
+                    </svg>
+                </div>
+                <p className="prod-name" id="epos_error_model_title">{LocalizedLanguage.messageTitle}</p>
                     <div className="modal-body center-center pt-5 pb-5">
                         <div className="plan-info">
-                            <p style={{fontSize:"2.69vw"}}>{LocalizedLanguage.selectAllAttributes}</p>
-                            <p style={{fontSize:"2.59vw"}}>{LocalizedLanguage.selectSpecificVariation}</p>
-                            <button style={{width:"50vw",marginTop:"20px"}} className="view-cart" type="button"  onClick={() => this.closePopupDisplayMessage()}>{LocalizedLanguage.Continue}
+                            <p style={{ fontSize: "2.69vw" }}>{LocalizedLanguage.selectAllAttributes}</p>
+                            <p style={{ fontSize: "2.59vw" }}>{LocalizedLanguage.selectSpecificVariation}</p>
+                            <button style={{ width: "50vw", marginTop: "20px" }} className="view-cart" type="button" onClick={() => this.closePopupDisplayMessage()}>{LocalizedLanguage.Continue}
                             </button>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         )
     }
